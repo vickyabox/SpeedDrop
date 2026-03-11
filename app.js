90a0cdc162bdf8fbf14fdd69702ae901ae24ed74
vickyabox <nirviikroy27@gmail.com>
2026-03-11 14:36:06 +0530

    Refactor parcel handling with offline/online events

--- app.js
+++ app.js
@@ -1,79 +1,13 @@
-function addParcel(){
+window.addEventListener("offline",()=>{
 
-let name=prompt("Customer Name")
-
-let address=prompt("Address")
-
-let parcel={
-name,
-address,
-lat:currentLat,
-lon:currentLon,
-photo:""
-}
-
-let list=getParcels()
-
-list.push(parcel)
-
-saveParcels(list)
-
-renderList()
-
-}
-
-function renderList(){
-
-let list=getParcels()
-
-let html=""
-
-list.forEach((p,i)=>{
-
-html+=`
-
-<div class="parcel"><b>${p.name}</b><br>
-
-${p.address}
-
-${p.photo?"<img src="${p.photo}">":""}
-
-<br><button onclick="takePhoto(${i})">
-Photo
-</button><button onclick="navigate(${i})">
-Navigate
-</button><button onclick="deleteParcel(${i})">
-Delete
-</button></div>`
+alert("Offline Mode")
 
 })
 
-document.getElementById(
-"parcelList"
-).innerHTML=html
+window.addEventListener("online",()=>{
 
-}
+alert("Internet Connected")
 
-function navigate(i){
+})
 
-let p=getParcels()[i]
-
-window.open(
-"https://www.google.com/maps/dir/?api=1&destination=${p.lat},${p.lon}"
-)
-
-}
-
-function deleteParcel(i){
-
-let list=getParcels()
-
-list.splice(i,1)
-
-saveParcels(list)
-
-renderList()
-
-}
-
-renderList()
+renderParcels()
